6b70dcb0147cfbf7c746b24e0f4fc57a9f7f2526
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-05-14 17:45:02 +0700

    Define colors, typography in theme

---
+++ FluxStore/src/themes/typography.ts
@@ -0,0 +1,20 @@
+export const fontWeights = {
+  regular: '400',
+  medium: '500',
+  semiBold: '600',
+  bold: '700',
+} as const;
+
+export const fontSize = {
+  xxl: 26,
+  xl: 25,
+  lg: 24,
+  md: 20,
+  base: 18,
+  sm: 16,
+  xs: 14,
+  xxs: 13,
+  tiny: 12,
+  micro: 11,
+  mini: 10,
+};
