833aa0240356018eafc840a5f93d19e11660ba24
Tien Nguyen <tien.nguyen@asnet.com.vn>
2025-05-23 16:09:30 +0700

    Add images

--- FluxStore/src/themes/images.ts
+++ FluxStore/src/themes/images.ts
@@ -6,4 +6,13 @@ export const Carousels = {
   special: require('@/assets/images/carousels/special.png'),
   favorite: require('@/assets/images/carousels/favorite.png'),
   relax: require('@/assets/images/carousels/relax.png'),
+  autumn: require('@/assets/images/carousels/autumn.png'),
+};
+
+export const Banners = {
+  colection: require('@/assets/images/banners/colection.png'),
+  beauty: require('@/assets/images/banners/beauty.png'),
+  fabulous: require('@/assets/images/banners/fabulous.png'),
+  shirts: require('@/assets/images/banners/shirts.png'),
+  dresses: require('@/assets/images/banners/dresses.png'),
 };
